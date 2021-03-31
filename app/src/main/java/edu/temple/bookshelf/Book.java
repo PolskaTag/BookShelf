bdba3d5e5ed6fddc9feb1a112ae9c270083d4dd7
Philip Lapinski <41872747+PolskaTag@users.noreply.github.com>
2021-03-31 18:17:57 -0400

    Created basic layout + Book Class The layouts still need to be configured to achieve the desired outcome.

---
+++ app/src/main/java/edu/temple/bookshelf/Book.java
@@ -0,0 +1,52 @@
+package edu.temple.bookshelf;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Book implements Parcelable {
+    private String title;
+    private String author;
+
+    // Default book
+    public Book(){}
+
+    // The one you'll most likely use
+    public Book(String title, String author){
+        this.title = title;
+        this.author = author;
+    }
+
+    protected Book(Parcel in){
+        title = in.readString();
+        author = in.readString();
+    }
+
+    public static final Creator<Book> CREATOR = new Creator<Book>(){
+        @Override
+        public Book createFromParcel(Parcel source) {
+            return new Book(source);
+        }
+
+        @Override
+        public Book[] newArray(int size) {
+            return new Book[size];
+        }
+    };
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    @Override
+    public int describeContents(){return 0;}
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(author);
+    }
+}
